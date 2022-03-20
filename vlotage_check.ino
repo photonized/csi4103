b2049f4e56121a1be15a1c2ae173c6846fc299bb
Min <zhangminregan@gmail.com>
2022-03-20 18:29:06 -0400

    Create vlotage_check.ino

---
+++ vlotage_check.ino
@@ -0,0 +1,13 @@
+const int read_analog_pin = ;#need to specify the pin of read analog.
+
+void setup(){
+	pinMode(read_analgo_pin, INPUT);
+	Serial.begin(9600)
+}
+
+void loop(){
+	OUTPUT = analogRead(gread_analgo_pin)
+	Serial.begin(OUTPUT)
+	dealy(1000)
+	
+}
